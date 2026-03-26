a3e4aef79ef73ae5fc6e56f5222cf087734a4e1f
ShriyanshRaut <rautshriyansh@gmail.com>
2026-03-26 18:43:40 +0530

    Update .gitignore to include additional file types and add cloudinary configuration in src/config/cloudinary.js; refactor src/index.js for consistency

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import { app } from "./app.js"
-import connectDB from "./config/db.js"
 
-const PORT = process.env.PORT || 8000
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+
+import { app } from "./app.js";
+import connectDB from "./config/db.js";
+
+const PORT = process.env.PORT || 8000;
 
 connectDB().then(() => {
-     app.on("error", (error) => {
-        console.log("ERROR:", error);
-        throw error;
-    });
+  app.on("error", (error) => {
+    console.log("ERROR:", error);
+    throw error;
+  });
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }).catch((error) => {
-    console.error("Server failed to start:", error)
-    process.exit(1)
-})
+  console.error("Server failed to start:", error);
+  process.exit(1);
+});
