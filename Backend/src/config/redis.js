4d031b1d6caf9bef2b8cc496a5103e4e1e9e95d7
ShriyanshRaut <rautshriyansh@gmail.com>
2026-04-21 11:38:57 +0530

    feat: implement dynamic Redis configuration for production and local environments

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,16 +1,38 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-});
+let redis = null;
 
-redis.on("connect", () => {
-  console.log("Redis connected ");
-});
+if (process.env.REDIS_URL) {
+  //  Production (Render / Cloud Redis)
+  redis = new Redis(process.env.REDIS_URL);
 
-redis.on("error", (err) => {
-  console.error("Redis error:", err);
-});
+  redis.on("connect", () => {
+    console.log(" Redis connected (cloud)");
+  });
+
+  redis.on("error", (err) => {
+    console.error("Redis error:", err);
+  });
+
+} else {
+  //  Local fallback
+  try {
+    redis = new Redis({
+      host: "127.0.0.1",
+      port: 6379,
+    });
+
+    redis.on("connect", () => {
+      console.log(" Redis connected (local)");
+    });
+
+    redis.on("error", (err) => {
+      console.warn(" Redis not available, skipping...");
+    });
+
+  } catch {
+    console.warn(" Redis disabled");
+  }
+}
 
 export default redis;
